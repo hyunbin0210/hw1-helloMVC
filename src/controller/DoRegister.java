ddef27e9a15b0c1330167d6f5cf88d90410881ef
hyunbin0210 <hyunbin0210@gamil.com>
2020-09-27 18:16:13 +0900

    Add Register function

---
+++ src/controller/DoRegister.java
@@ -0,0 +1,65 @@
+package controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.Customer;
+import service.CustomerService;
+
+/**
+ * Servlet implementation class DoRegister
+ */
+@WebServlet("/doRegister")
+public class DoRegister extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public DoRegister() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		String id = request.getParameter("id");
+		String password = request.getParameter("password");
+		String name = request.getParameter("name");
+		String gender = request.getParameter("gender");
+		String email = request.getParameter("email");
+		
+		CustomerService service = (CustomerService) CustomerService.getInstance();		
+		
+		String page;
+		if (service.findCustomer(id) != null) 
+			page = "/view/error.jsp";
+		else {			
+			Customer customer = new Customer(id, password, name, gender, email);
+			service.addCustomer(customer);
+			page = "/view/registerSuccess.jsp";
+			request.setAttribute("customer", customer);
+		}
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher(page);
+		dispatcher.forward(request, response);
+	}
+
+}
